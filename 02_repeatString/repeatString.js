d0dace41de0b8a821a2c5bdb4859b10f958b74c6
laiacbs18 <laiacbs18@gmail.com>
2023-06-14 13:27:38 -0400

    Finished first 2 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, count) {
+    if(count >= 0){
+        return word.repeat(Math.abs(Math.round(count)));
+    }else{
+        return 'ERROR';
+    }
+    
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(input) {
+    var myInput =  Array.from(input);
+    input = '';    
 
+    for(var i = myInput.length - 1; i >= 0; i--){
+        input += myInput[i];
+    }
+
+    return input;
 };
 
 // Do not edit below this line
